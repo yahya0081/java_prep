17a983d09cf0b03b210b7e81da1edb5914c564e2
Yahya ayaz <159167014+yahya0081@users.noreply.github.com>
2024-09-02 13:22:07 +0000

    config done

---
+++ prep_01/data2.js
@@ -0,0 +1,19 @@
+//alert("hello gujjar")//we are usiing node js not browser
+let name = "yahya"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
---
+++ prep_01/variable.js
@@ -0,0 +1,10 @@
+const account_id =123456
+let account_email="yahya123ayaz@gmail.com"
+var accountPassword="12345678"
+account_city="lahore"
+//account_id=2 
+ account_email="gujjar@gmail"
+ accountPassword="987654"
+ account_city="karachi"
+console.log(account_id);
+console.table([account_id,account_email,account_city,accountPassword]);
